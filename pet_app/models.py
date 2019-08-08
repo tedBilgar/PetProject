3550d8f43d00d6fc98d6b999ab867feedbb18e63
Denis Urakanov <mcquicklyss@gmail.com>
2019-08-08 23:49:12 +0500

    adding Owner Model with Many to One Relation

--- pet_app/models.py
+++ pet_app/models.py
@@ -9,3 +9,17 @@ class Cat(models.Model):
 
     def __str__(self):
         return "{} - {}".format(self.name, self.home)
+
+
+class Owner(models.Model):
+    cat = models.ForeignKey(Cat, related_name='onwers_of_cat', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, null=True)
+    money = models.IntegerField()
+    occupation = models.CharField(max_length=100, null=False)
+
+    class Meta:
+        unique_together = ['cat', 'name']
+        ordering = ['name']
+
+    def __str__(self):
+        return '%d: %s' % (self.money, self.name)
