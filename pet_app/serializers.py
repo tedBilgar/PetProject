3ef987cbb95b16156a6d17581ad77f2471e7ec7d
Denis Urakanov <mcquicklyss@gmail.com>
2019-08-12 22:50:07 +0500

    Many to Many #5 (adding ID field for all models)

--- pet_app/serializers.py
+++ pet_app/serializers.py
@@ -10,26 +10,26 @@ class CatSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Cat
-        fields = ("name", "home", 'owners_of_cat')
+        fields = ('id', "name", "home", 'owners_of_cat')
 
 
 class OwnerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Owner
-        fields = ('cat', 'name', 'money', 'occupation')
+        fields = ('id', 'cat', 'name', 'money', 'occupation')
 
 
 class ToySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Toy
-        fields = ('toy_name', 'developer')
+        fields = ('id', 'toy_name', 'developer')
 
 
 class CatToySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CatToyRelation
-        fields = ('cat', 'toy', 'major')
+        fields = ('id', 'cat', 'toy', 'major')
 
 
